3aaa81c74a217733465c67d369551d65856a98f0
Temir Umurzakov <temir@umurzakov.com>
2025-12-10 22:16:21 +0600

    fugitive added

---
+++ lua/plugins/fugitive.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "tpope/vim-fugitive",
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup({
+        -- можно вообще без настроек
+      })
+    end,
+  },
+}
