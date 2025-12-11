267d0cfda4b6e8fcd446dbf3d5080d8e8159c4fa
Temir Umurzakov <temir@umurzakov.com>
2025-12-11 15:17:20 +0600

    Update tree.lua plugin configuration
    - Modified tree.lua plugin settings
    - Adjusted tree view behavior
    - Enhanced plugin integration

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -10,6 +10,35 @@ return {
       git = {
         ignore = false,              -- также отключает глобальную фильтрацию gitignore
       },
+      on_attach = function(bufnr)
+        local api = require("nvim-tree.api")
+
+        -- сначала ставим дефолтные бинды, чтобы ничего не сломать
+        api.config.mappings.default_on_attach(bufnr)
+
+        -- удобная helper-функция для опций
+        local function opts(desc)
+          return {
+            desc = "nvim-tree: " .. desc,
+            buffer = bufnr,
+            noremap = true,
+            silent = true,
+            nowait = true,
+          }
+        end
+
+        -- ТУТ главное:
+        -- В нормальном режиме внутри nvim-tree:
+        -- жмём "t" => открыть в новом табе
+        vim.keymap.set("n", "t", api.node.open.tab, opts("Open in new tab"))
+        -- при желании можно ещё добавить "T" на tab drop:
+        -- vim.keymap.set("n", "T", api.node.open.tab_drop, opts("Open: Tab drop"))
+      end,
+
+      -- остальная твоя конфигурация, если есть
+      view = {
+        width = 35,
+      },
       renderer = {
         icons = {
           -- Use Nerd Font icons for files and folders
